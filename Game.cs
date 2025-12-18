46c093076319fef2fae084a3f50a2d2f77bd40a9
Andrew Abramajtys <anab639@student.oaisd.org>
2025-12-18 19:44:14 +0000

    added killswitch

--- Game.cs
+++ Game.cs
@@ -2,37 +2,71 @@ namespace Simulator;
 
 public static class Game
 {
-    public int money = 0;
-    public int rating = 0;
-    public string player = "";
+    public static int money = 0;
+    public static int rating = 0;
+    public static string player = "";
+    public static string response = "";
+    public static string lResponse = "";
 
-    public static void GameInfo()
+    public static void input()
+    {
+        response = Console.ReadLine();
+        lResponse = response.ToLower();
+        if (lResponse == "end")
+        {
+            Environment.Exit(0);
+        }
+    }
+    
+    public static void PlayerInfo()
     {
         Console.WriteLine("Your current balance is $" + money);
         Console.WriteLine("Your current rating is " + rating);
         Console.WriteLine("Name: " + player);
+        Thread.Sleep(1000);
+        Console.WriteLine("Press anything to continue");
+        input();
+        Thread.Sleep(1000);
+        Menu();
     }
 
     public static void NameInquiry()
     {
-        Console.WriteLine("What is your name?")
+        Console.WriteLine("What is your name?");
         player = Console.ReadLine();
+        if (player == "end")
+        {
+            Environment.Exit(0);
+        }
     }
 
     public static void GameStart()
     {
         Console.WriteLine("Welcome to unnamed simulator game");
-        Thread.Wait(1000);
+        Thread.Sleep(1000);
         Console.WriteLine("Allan please add details.");
-        Thread.Wait(1500);
+        Thread.Sleep(1500);
         Console.WriteLine("Press anything to continue");
-        Console.ReadLine();
+        input();
     }
 
     public static void Menu()
     {
-        Console.WriteLine("What do you want to do?")
-        Thread.Wait(1000);
-        Console.WriteLine("To view your current player info, say ");
+        Console.WriteLine("What do you want to do?");
+        Thread.Sleep(1000);
+        Console.WriteLine("To view your current player info, say ''Info''");
+        Thread.Sleep(1000);
+        input();
+        if (lResponse == "info")
+        {
+            PlayerInfo();
+        }
+    }
+
+    public static void Test()
+    {
+        GameStart();
+        NameInquiry();
+        Menu();
     }
 }
